8f0152ebb88d9cd3a5f2e9f2721fa0d8d8cf6b7c
Matheus Anthony Pereira Abreu <matheus.anthony.pa@outlook.com>
2023-10-24 14:12:44 -0300

    feat: Aula 16

--- EstudosAngular/meu-primeiro-projeto/src/app/app.component.ts
+++ EstudosAngular/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,22 +1,16 @@
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <app-title title = "Olá Mundo!" ></app-title>
-    <router-outlet></router-outlet>`
+  template: '<router-outlet></router-outlet>'
 })
-export class AppComponent implements OnInit, OnChanges {
+export class AppComponent implements OnInit {
   constructor() {
   }
 
   ngOnInit(): void {
-    // setTimeout(() => {
-    //   console.log(1);
-    // }, 5000)
-  }
-
-  ngOnChanges(): void {
-    
+    setTimeout(() => {
+      console.log(1);
+    }, 5000)
   }
 }
--- EstudosAngular/meu-primeiro-projeto/src/app/title/title.component.ts
+++ EstudosAngular/meu-primeiro-projeto/src/app/title/title.component.ts
@@ -1,24 +1,10 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css']
 })
-
-export class TitleComponent implements OnInit, OnChanges{
-  @Input() public title:string = "Bem Vindo!";
-
-  constructor() { }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-
-  ngOninit(): void{ }
-
-  ngOnChanges(): void { 
-    alert("Foi alterado com sucesso !")
-  }
-
-
+export class TitleComponent{
+  public title:string = "Bem Vindo!";
 }
