b19bfd838a3dd0900673dd68015bc741b1a2b868
Matheus Anthony Pereira Abreu <matheus.anthony.pa@outlook.com>
2023-11-09 15:30:45 -0300

    feat: projeto firebase

---
+++ Projetos/Firebase/angularfirebase-authentication/src/app/components/verify-email/verify-email.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/services/auth';
+
+@Component({
+  selector: 'app-verify-email',
+  templateUrl: './verify-email.component.html',
+  styleUrls: ['./verify-email.component.scss']
+})
+
+export class VerifyEmailComponent implements OnInit{
+  
+  constructor (
+    public authService : AuthService
+  ) { }
+
+  ngOnInit(){
+    
+  }
+}
